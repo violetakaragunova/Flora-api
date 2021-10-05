e96859e4081ad103fe6ff9db5572d4db884e1f8c
Violeta Karagunova <violetak@iborn.net>
2021-10-05 12:36:25 +0200

    Add Migrations/20210824132456_AddingPlantFamilyNameAtributeToPlantEntity.cs

---
+++ DataAccessLayer/Migrations/20210824132456_AddingPlantFamilyNameAtributeToPlantEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlantTrackerAPI.DataAccessLayer.Migrations
+{
+    public partial class AddingPlantFamilyNameAtributeToPlantEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PlantFamilyName",
+                table: "Plants",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlantFamilyName",
+                table: "Plants");
+        }
+    }
+}
